063d79db9c20c6aad452d7467d05e455aeeea0bd
James Wienecke <james.a.wienecke@gmail.com>
2022-03-07 23:24:54 -0600

    Basic project setup.

---
+++ src/main/java/com/jamesw/jacksontesting/JacksonTestingApplication.java
@@ -0,0 +1,13 @@
+package com.jamesw.jacksontesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacksonTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JacksonTestingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jamesw/jacksontesting/JacksonTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamesw.jacksontesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacksonTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
